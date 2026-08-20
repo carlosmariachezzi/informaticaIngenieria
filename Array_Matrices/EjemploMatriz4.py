0371a8deebb794e07fbf69c15008e02a8e880011
carlosmariachezzi <carlosmariachezzi@gmail.com>
2026-08-20 18:06:51 -0300

    Add files via upload

---
+++ Array_Matrices/EjemploMatriz4.py
@@ -0,0 +1,19 @@
+# Cargar matriz de str (caracteres) usando NumPy (dtype=str)
+import numpy as np
+
+# Dimensiones
+filas = int(input("Ingrese la cantidad de filas: "))
+columnas = int(input("Ingrese la cantidad de columnas: "))
+
+# Inicializar matriz con cadenas vacías
+matriz = np.full((filas, columnas), '', dtype=str)
+
+# Cargar datos
+for i in range(filas):
+    for j in range(columnas):
+        valor = input(f"Ingrese una palabra para [{i}][{j}]: ")
+        matriz[i, j] = valor
+
+# Mostrar la matriz
+print("Matriz ingresada:")
+print(matriz)

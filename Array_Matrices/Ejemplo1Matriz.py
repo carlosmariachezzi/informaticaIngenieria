c97e96a1370dcbb60ab6c5ac7e4a1001822a0f4d
carlosmariachezzi <carlosmariachezzi@gmail.com>
2026-08-20 17:00:24 -0300

    Add files via upload

---
+++ Array_Matrices/Ejemplo1Matriz.py
@@ -0,0 +1,11 @@
+# Crear una matriz con np.array() a partir de datos ya definidos en listas
+import numpy as np
+
+# Definir matriz directamente
+matriz = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+
+print("Matriz definida manualmente:")
+print(matriz)
